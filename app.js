8c14df4b0360ebb711719e750b2d10416f37d846
brick <brick.c.yang@gmail.com>
2017-09-14 15:29:51 +0800

    logger -> coreLogger

--- app.js
+++ app.js
@@ -9,18 +9,26 @@ function createMongo(config, app) {
   const client = new MongoDB(config);
 
   client.on('connect', () => {
-    app.logger.info('[egg-mongo] Connect success on mongo://%s:%s/%s.',
-      config.host, config.port, config.name);
+    app.coreLogger.info(
+      '[egg-mongo] Connect success on mongo://%s:%s/%s.',
+      config.host,
+      config.port,
+      config.name
+    );
   });
   /* istanbul ignore next */
   client.on('error', error => {
-    app.logger.warn('[egg-mongo] Connect fail on mongo://%s:%s/%s.',
-      config.host, config.port, config.name);
-    app.logger.error(error);
+    app.coreLogger.warn(
+      '[egg-mongo] Connect fail on mongo://%s:%s/%s.',
+      config.host,
+      config.port,
+      config.name
+    );
+    app.coreLogger.error(error);
   });
 
   app.beforeStart(async () => {
-    app.logger.info('[egg-mongo] Connecting MongoDB...');
+    app.coreLogger.info('[egg-mongo] Connecting MongoDB...');
     await client.connect();
   });
 
